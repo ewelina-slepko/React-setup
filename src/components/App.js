adea748d9f9e461e05a48c71128270b2351ac50a
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-05-03 22:09:50 +0200

    made React JS boilerplate
    - Webpack 4,
    - Babel, 
    - Webpack Dev Server,
    - NodeJS,

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Create new App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
