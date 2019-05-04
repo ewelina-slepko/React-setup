f016387a9c0e23e33139b322283cd6576f8aa3b7
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-05-04 23:21:42 +0200

    Added clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -23,6 +24,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/index.html'
         }),
+        new CleanWebpackPlugin(),
         new WorkboxPlugin.GenerateSW()
     ]
 }
